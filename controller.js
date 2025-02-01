63fcd5c1e6de0936a571ab9b3dbc0292521e3ab6
InsituPeter <insitupeter@gmail.com>
2025-02-01 18:42:05 +0100

    Final edit

--- controller.js
+++ controller.js
@@ -1,7 +1,7 @@
 require("dotenv").config()
 const basicInfo =   (req, res)=>{
   if (!process.env.EMAIL || !process.env.GITHUB_URL){
-  
+   res.send ('email or github repo missing')
   }
   res.status(200).json({
       email: process.env.EMAIL,
